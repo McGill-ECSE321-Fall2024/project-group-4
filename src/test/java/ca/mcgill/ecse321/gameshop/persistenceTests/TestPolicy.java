3a5d0da8936189e8863c1cefcb6e34f1d6ad5130
tnhunter <tnhunter2004@gmail.com>
2024-10-08 23:38:43 -0400

    Setup an example test for persistence layer - Removed umple model classes - Created a CRUD repository for Policy class and implemented tests

--- src/test/java/ca/mcgill/ecse321/gameshop/persistenceTests/TestPolicy.java
+++ src/test/java/ca/mcgill/ecse321/gameshop/persistenceTests/TestPolicy.java
@@ -1,7 +1,7 @@
 package ca.mcgill.ecse321.gameshop.persistenceTests;
 
 import ca.mcgill.ecse321.gameshop.persistence.Policy;
-import ca.mcgill.ecse321.gameshop.repositories.PolicyRepository;
+import ca.mcgill.ecse321.gameshop.DAO.PolicyRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +23,7 @@ public class TestPolicy {
     @Test
     void testCreatePolicy() {
         //create policy
-        Policy policy = new Policy();
-        policy.setDescription("Nothing allowed");
+        Policy policy = new Policy("Nothing allowed");
 
 
         //save policy
@@ -32,13 +31,45 @@ public class TestPolicy {
         int id = policy.getId();
 
         //read from database
-        Policy loadedPolicy = policyRepository.findById(id).get();
+        Policy loadedPolicy = policyRepository.findById(id).orElse(null);
 
 
         //Assert correct response
         assertNotNull(loadedPolicy);
         assertEquals(policy.getDescription(), loadedPolicy.getDescription());
 
+
+    }
+    @Test
+    void testUpdatePolicy() {
+        //create and save policy
+        Policy policy = new Policy("Nothing allowed");
+        policy = policyRepository.save(policy);
+        int id = policy.getId();
+
+        //save policy
+        policy.setDescription("Updated description");
+        policy = policyRepository.save(policy);
+
+        Policy loadedPolicy = policyRepository.findById(id).orElse(null);
+        assertNotNull(loadedPolicy);
+        assertEquals(1, policyRepository.count());
+        assertEquals(policy.getDescription(), loadedPolicy.getDescription());
+
+    }
+
+    @Test
+    void testDeletePolicy() {
+        //create and save policy
+        Policy policy = new Policy("Nothing allowed");
+        policy = policyRepository.save(policy);
+        int id = policy.getId();
+
+        //delete policy
+        policyRepository.deleteById(id);
+
+
+        assertEquals(0, policyRepository.count());
 
     }
 }
