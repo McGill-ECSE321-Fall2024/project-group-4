f3a85ebaf8af932ef57884a11a60b03fb3438c2f
tnhunter <tnhunter2004@gmail.com>
2024-10-10 19:53:47 -0400

    Created tests for persistence layer

---
+++ src/test/java/ca/mcgill/ecse321/gameshop/persistenceTests/TestEmployee.java
@@ -0,0 +1,54 @@
+package ca.mcgill.ecse321.gameshop.persistenceTests;
+
+import ca.mcgill.ecse321.gameshop.DAO.EmployeeRepository;
+import ca.mcgill.ecse321.gameshop.persistence.Employee;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+/**
+ * Author : Tarek Namani
+ */
+@SpringBootTest
+public class TestEmployee {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    Employee employee = new Employee();
+
+
+    @AfterEach
+    public void tearDown() {
+        employeeRepository.deleteAll();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        employee = new Employee();
+        employee.setActive(true);
+        employee.setPassword("Unsafe Password");
+        employee.setUsername("Bob joe");
+    }
+    /**
+     * Author : Tarek Namani
+     * Description : Tests saving and loading a Employee object from the database
+     */
+    @Test
+    public void testSaveAndLoadEmployee() {
+        //save
+        employeeRepository.save(employee);
+
+        //load
+        Employee loadedEmployee = employeeRepository.findById(employee.getId());
+
+        //compare
+        assertEquals(employee.getId(),loadedEmployee.getId());
+        assertEquals(employee.getPassword(),loadedEmployee.getPassword());
+        assertEquals(employee.getUsername(),loadedEmployee.getUsername());
+        assertEquals(employee.isActive(),loadedEmployee.isActive());
+    }
+}
---
+++ src/test/java/ca/mcgill/ecse321/gameshop/persistenceTests/TestGameRequest.java
@@ -0,0 +1,78 @@
+package ca.mcgill.ecse321.gameshop.persistenceTests;
+
+import ca.mcgill.ecse321.gameshop.DAO.EmployeeRepository;
+import ca.mcgill.ecse321.gameshop.DAO.GameRepository;
+import ca.mcgill.ecse321.gameshop.DAO.GameRequestRepository;
+import ca.mcgill.ecse321.gameshop.persistence.Employee;
+import ca.mcgill.ecse321.gameshop.persistence.Game;
+import ca.mcgill.ecse321.gameshop.persistence.GameRequest;
+import ca.mcgill.ecse321.gameshop.persistence.RequestStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Author : Tarek Namani
+ */
+@SpringBootTest
+public class TestGameRequest {
+
+    @Autowired
+    private GameRequestRepository gameRequestRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private GameRepository gameRepostitory;
+
+    private Game game = new Game("testame","testDescription","testCover",50.00F, true, 100);
+    private Employee employee = new Employee("Bob","Joe",true);
+    private String review = "Test review";
+    private GameRequest gameRequest;
+
+
+    @AfterEach
+    public void clearRepository() {
+        gameRequestRepository.deleteAll();
+        gameRepostitory.deleteAll();
+        employeeRepository.deleteAll();
+
+    }
+
+    @BeforeEach
+    public void setup() {
+        gameRequest = new GameRequest();
+        gameRequest.setGame(game);
+        gameRequest.setRequestor(employee);
+        gameRequest.setExternalReview(review);
+        gameRequest.setStatus(RequestStatus.PENDING);
+    }
+
+
+    /**
+     * Author : Tarek Namani
+     * Description : Tests saving and loading a GameRequest object from the database
+     */
+    @Test
+    public void testSaveAndLoadGameRequest() {
+        //save the gameRequest
+        employeeRepository.save(employee);
+        gameRepostitory.save(game);
+        gameRequestRepository.save(gameRequest);
+
+        //load the gameRequest
+        GameRequest loadedRequest = gameRequestRepository.findById(gameRequest.getId()).orElse(null);
+
+        //check the contents
+        assertEquals(gameRequest.getGame().getName(),loadedRequest.getGame().getName());
+        assertEquals(gameRequest.getRequestor().getUsername(),loadedRequest.getRequestor().getUsername());
+        assertEquals(gameRequest.getStatus(),loadedRequest.getStatus());
+        assertEquals(gameRequest.getExternalReview(),loadedRequest.getExternalReview());
+
+    }
+
+
+}
