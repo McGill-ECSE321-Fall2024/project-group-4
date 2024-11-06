ae7c494feb9ffbcf7dcd9b9a284b6e41b4463d22
Tatok-n <tnhunter2004@gmail.com>
2024-11-06 15:46:12 -0500

    Added all associated methods for PurchaseManagementService Still needs to be tested

--- src/main/java/ca/mcgill/ecse321/gameshop/serviceClasses/PurchaseManagementService.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/serviceClasses/PurchaseManagementService.java
@@ -1,15 +1,15 @@
 package ca.mcgill.ecse321.gameshop.serviceClasses;
 
 import ca.mcgill.ecse321.gameshop.DAO.*;
-import ca.mcgill.ecse321.gameshop.model.Customer;
-import ca.mcgill.ecse321.gameshop.model.Purchase;
-import ca.mcgill.ecse321.gameshop.model.Reply;
-import ca.mcgill.ecse321.gameshop.model.Review;
+import ca.mcgill.ecse321.gameshop.model.*;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.filter.OrderedHiddenHttpMethodFilter;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class PurchaseManagementService {
@@ -22,6 +22,12 @@ public class PurchaseManagementService {
     private PurchaseRepository purchaseRepository;
     @Autowired
     private ReplyRepository replyRepository;
+    @Autowired
+    private GameRepository gameRepository;
+    @Autowired
+    private CreditCardRepository creditCardRepository;
+    @Autowired
+    private AddressRepository addressRepository;
 
     @Transactional
     public Review findReviewById(int id) {
@@ -50,6 +56,24 @@ public class PurchaseManagementService {
     }
 
     @Transactional
+    public CreditCard findByCreditCardId(int creditCardId) {
+        Optional<CreditCard> optionalCreditCard = creditCardRepository.findById(creditCardId);
+        if (optionalCreditCard.isPresent()) {
+            return optionalCreditCard.get();
+        }
+        throw new IllegalArgumentException("No Credit Card found with id " + creditCardId);
+    }
+
+    @Transactional
+    public Address findAddressById(int addressId) {
+        Optional<Address> optAdress = addressRepository.findById(addressId);
+        if (optAdress.isPresent()) {
+            return optAdress.get();
+        }
+        throw new IllegalArgumentException("No Address found with id " + addressId);
+    }
+
+    @Transactional
     public Review createReview(int rating, String text, int purchaseId) {
 
         Purchase purchase = findPurchaseById(purchaseId);
@@ -58,6 +82,20 @@ public class PurchaseManagementService {
 
         return review;
 
+    }
+
+
+
+    @Transactional
+    public CreditCard createCreditCard(int cardNumber, int cvv, LocalDate expiryDate, String customerEmail, int addressId) {
+        Customer customer = findCustomerByEmail(customerEmail);
+        Address billingAddress = findAddressById(addressId);
+
+        CreditCard creditCard = new CreditCard(cardNumber, cvv, expiryDate, customer, billingAddress);
+
+        creditCardRepository.save(creditCard);
+
+        return creditCard;
     }
 
     @Transactional
@@ -99,6 +137,71 @@ public class PurchaseManagementService {
 
         replyRepository.save(reply);
         reviewRepository.save(review);
+
+
+    }
+
+    @Transactional
+    public void checkout(String customerEmail) {
+        Customer customer = findCustomerByEmail(customerEmail);
+
+        Set<Game> gamesInCart = customer.getCopyCart();
+        if (gamesInCart.isEmpty()) throw new IllegalArgumentException("Cannot checkout an empty cart!");
+        customer.getCopyCart().forEach(game -> {
+            customer.removeGameFromCart(game);
+            game.removeInCartOf(customer);
+            gameRepository.save(game);
+        });
+        customerRepository.save(customer);
+    }
+
+    @Transactional
+    public float getCartPrice(String customerEmail) {
+        Customer customer = findCustomerByEmail(customerEmail);
+        long price = customer.getCopyCart().stream().mapToLong(game -> (long) game.getPrice()).sum();
+        return (float) price;
+    }
+
+
+    @Transactional
+    public Set<CreditCard> viewCustomerCreditCards(String email) {
+        Customer customer = findCustomerByEmail(email);
+        return customer.getCopyofCreditCards();
+    }
+
+    public void addCreditCardToCustomerWallet(String customerEmail, int creditCardId) {
+        Customer customer = findCustomerByEmail(customerEmail);
+        CreditCard creditCard = findByCreditCardId(creditCardId);
+
+        creditCard.setCustomer(customer);
+        customer.addCreditCardToWallet(creditCard);
+
+        creditCardRepository.save(creditCard);
+        customerRepository.save(customer);
+    }
+
+    @Transactional
+    public void removeCreditCardFromWallet(String customerEmail, int creditCardId) {
+        Customer customer = findCustomerByEmail(customerEmail);
+        CreditCard creditCard = findByCreditCardId(creditCardId);
+
+
+        if (customer.getCopyofCreditCards().isEmpty()) {
+            throw new IllegalArgumentException("Customer doesn't have credit cards!");
+        }
+
+        if (customer.getCopyofCreditCards().contains(creditCard) && creditCard.getCustomer() == customer) {
+            customer.getCopyofCreditCards().remove(creditCard);
+            creditCard.setCustomer(null);
+            creditCardRepository.save(creditCard);
+            customerRepository.save(customer);
+        }
+
+        if (!customer.getCopyofCreditCards().contains(creditCard)) {
+            throw new IllegalArgumentException("Customer does now own credit card : " + creditCard.getCardNumber());
+        }
+
+        throw new IllegalArgumentException("Credit card is not associated to customer : " + customer.getUsername());
 
 
     }
