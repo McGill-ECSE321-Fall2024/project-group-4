2e9197e1a36c70cc0c632714e8071cea25202576
Jake <46657160+jacobkogut@users.noreply.github.com>
2024-11-08 21:26:25 -0500

    Added Category & Cart REST controller endpoints & DTOs

--- src/main/java/ca/mcgill/ecse321/gameshop/serviceClasses/GameManagementService.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/serviceClasses/GameManagementService.java
@@ -6,7 +6,9 @@ import ca.mcgill.ecse321.gameshop.DAO.GameRepository;
 import ca.mcgill.ecse321.gameshop.model.Category;
 import ca.mcgill.ecse321.gameshop.model.Customer;
 import ca.mcgill.ecse321.gameshop.model.Game;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +24,7 @@ public class GameManagementService {
 
     @Autowired
     private CustomerRepository customerRepository;
-
+    @Transactional
     public void addGameToCart(int customerId, int gameId){
         Game gameToAdd = gameRepository.findById(gameId).orElseThrow(()-> new EntityNotFoundException("Game to add to cart not found"));
         Customer customer = customerRepository.findById(customerId).orElseThrow(()-> new EntityNotFoundException("Customer to add game to cart of not found"));
@@ -34,7 +36,7 @@ public class GameManagementService {
         Customer customer = customerRepository.findById(customerId).orElseThrow(()-> new EntityNotFoundException("Customer to add game to cart of not found"));
         return customer.getCopyCart();
     }
-
+    @Transactional
     public void removeGameFromCart(int customerId, int gameId){
         Game gameToRemove = gameRepository.findById(gameId).orElseThrow(()-> new EntityNotFoundException("Game to remove from cart not found"));
         Customer customer = customerRepository.findById(customerId).orElseThrow(()-> new EntityNotFoundException("Customer to remove game from cart of not found"));
@@ -44,12 +46,15 @@ public class GameManagementService {
         }
         customerRepository.save(customer);
     }
-
+    @Transactional
     public void createCategory(String name){
+        if(categoryRepo.findByName(name).isPresent()){
+            throw new EntityExistsException("Category to add already exists");
+        }
         Category category = new Category(name);
         categoryRepo.save(category);
     }
-
+    @Transactional
     public void deleteCategory(String name){
         var category = categoryRepo.findByName(name).orElseThrow(()-> new EntityNotFoundException("Category to delete was not found"));
         categoryRepo.delete(category);
