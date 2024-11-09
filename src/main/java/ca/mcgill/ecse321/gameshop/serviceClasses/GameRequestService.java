2584df306cff0d377ec406a090a0f5fd0a1ba3a3
Camomille103 <camille.pouliot@mail.mcgill.ca>
2024-11-09 13:08:31 -0500

    PromotionService V1

--- src/main/java/ca/mcgill/ecse321/gameshop/serviceClasses/GameRequestService.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/serviceClasses/GameRequestService.java
@@ -6,10 +6,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class GameRequestService {
