c68b54c39fe31a22ff8b06c42e0d05f677f582a2
Camomille103 <camille.pouliot@mail.mcgill.ca>
2024-11-10 15:42:24 -0500

    GameRequestService merged in GameManagementService

--- src/main/java/ca/mcgill/ecse321/gameshop/serviceClasses/GameManagementService.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/serviceClasses/GameManagementService.java
@@ -1,17 +1,14 @@
 package ca.mcgill.ecse321.gameshop.serviceClasses;
 
-import ca.mcgill.ecse321.gameshop.DAO.CategoryRepository;
-import ca.mcgill.ecse321.gameshop.DAO.CustomerRepository;
-import ca.mcgill.ecse321.gameshop.DAO.GameRepository;
-import ca.mcgill.ecse321.gameshop.model.Category;
-import ca.mcgill.ecse321.gameshop.model.Customer;
-import ca.mcgill.ecse321.gameshop.model.Game;
+import ca.mcgill.ecse321.gameshop.DAO.*;
+import ca.mcgill.ecse321.gameshop.model.*;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -24,6 +21,13 @@ public class GameManagementService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private GameRequestRepository gameRequestRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Transactional
     public void addGameToCart(int customerId, int gameId){
         Game gameToAdd = gameRepository.findById(gameId).orElseThrow(()-> new EntityNotFoundException("Game to add to cart not found"));
@@ -64,5 +68,89 @@ public class GameManagementService {
         var category = categoryRepo.findByName(name).orElseThrow(()-> new EntityNotFoundException("Category to get games in was not found"));
         return category.getCopyInCategory();
 
+    }
+
+    @Transactional
+    public GameRequest findGameRequestById(int id){
+        Optional<GameRequest> gameRequest = gameRequestRepository.findById(id);
+        if (gameRequest.isPresent()) {
+            return gameRequest.get();
+        }
+        throw new IllegalArgumentException("No Game Request found");
+    }
+
+    @Transactional
+    public Game findGameById(int id){
+        Optional<Game> game = gameRepository.findById(id);
+        if (game.isPresent()) {
+            return game.get();
+        }
+        throw new IllegalArgumentException("No Game found");
+    }
+
+    @Transactional
+    public Employee findEmployeeById(int id){
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent()) {
+            return employee.get();
+        }
+        throw new IllegalArgumentException("No Employee found");
+    }
+
+    @Transactional
+    public GameRequest createGameRequest(String externalReview, int gameId, int employeeId){
+
+        Game game = findGameById(gameId);
+
+        if(game.isActive()) {
+            throw new IllegalArgumentException("Game is already active");
+        }
+
+        Employee employee = findEmployeeById(employeeId);
+
+        GameRequest gameRequest = new GameRequest(externalReview, RequestStatus.PENDING, employee, game);
+
+        employee.addGameRequest(gameRequest);
+
+        gameRequestRepository.save(gameRequest);
+        employeeRepository.save(employee);
+
+        return gameRequest;
+
+    }
+
+    @Transactional
+    public GameRequest approveGameRequest(int gameRequestId){
+        GameRequest gameRequest = findGameRequestById(gameRequestId);
+
+        if(gameRequest.getStatus() == RequestStatus.APPROVED){
+            throw new IllegalArgumentException("Game Request is already approved");
+        }
+        if(gameRequest.getStatus() == RequestStatus.DENIED){
+            throw new IllegalArgumentException("Game Request is already denied");
+        }
+
+        Game game = gameRequest.getGame();
+        game.setActive(true);
+        gameRepository.save(game);
+        gameRequest.setStatus(RequestStatus.APPROVED);
+        gameRequestRepository.save(gameRequest);
+        return gameRequest;
+    }
+
+    @Transactional
+    public GameRequest rejectGameRequest(int gameRequestId){
+        GameRequest gameRequest = findGameRequestById(gameRequestId);
+
+        if(gameRequest.getStatus() == RequestStatus.APPROVED){
+            throw new IllegalArgumentException("Game Request is already approved");
+        }
+        if(gameRequest.getStatus() == RequestStatus.DENIED){
+            throw new IllegalArgumentException("Game Request is already denied");
+        }
+
+        gameRequest.setStatus(RequestStatus.DENIED);
+        gameRequestRepository.save(gameRequest);
+        return gameRequest;
     }
 }
