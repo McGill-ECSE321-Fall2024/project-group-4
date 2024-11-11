e51c7f6f35139947a6327cb231e638108a1e0af4
tnhunter <tnhunter2004@gmail.com>
2024-11-10 19:50:00 -0500

    Fix : fixed some failing tests, added Spy to emulate different IDs

--- src/main/java/ca/mcgill/ecse321/gameshop/DAO/ManagerRepository.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/DAO/ManagerRepository.java
@@ -4,7 +4,10 @@ import ca.mcgill.ecse321.gameshop.model.Manager;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ManagerRepository extends CrudRepository<Manager, Integer> {
+    Optional<Manager> findById(int id);
 
 }
