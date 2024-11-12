54b2335792f913d95a4af31d0fcd0e407eb4080f
anagordon <anbgordon@gmail.com>
2024-11-12 16:27:47 -0500

    createCustomer integration test

--- src/main/java/ca/mcgill/ecse321/gameshop/dto/CustomerDTO.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/dto/CustomerDTO.java
@@ -3,6 +3,8 @@ package ca.mcgill.ecse321.gameshop.dto;
 import ca.mcgill.ecse321.gameshop.model.Customer;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -27,6 +29,22 @@ public record CustomerDTO(String username,
         ,customer.getCopyofCreditCards().stream().map(CreditCardDTO::new).collect(Collectors.toSet())
         ,customer.getCopyLikedReviews().stream().map(ReviewDTO::new).collect(Collectors.toSet())
                 ,customer.getCopyPurchases().stream().map(PurchaseDTO::new).collect(Collectors.toSet()));
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static List<CustomerDTO> convertToDto(List<Customer> customers) {
+        List<CustomerDTO> customerDto = new ArrayList<CustomerDTO>(customers.size());
+        for (Customer customer : customers) {
+            customerDto.add(new CustomerDTO(customer));
+        }
+        return customerDto;
     }
 
 }
