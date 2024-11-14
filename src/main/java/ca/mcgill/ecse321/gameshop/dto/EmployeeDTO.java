163b30d737ca2b05297706cdfa44f3da4d602538
anagordon <anbgordon@gmail.com>
2024-11-13 23:47:42 -0500

    Update EmployeeDTO.java

--- src/main/java/ca/mcgill/ecse321/gameshop/dto/EmployeeDTO.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/dto/EmployeeDTO.java
@@ -5,9 +5,10 @@ import ca.mcgill.ecse321.gameshop.model.Employee;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public record EmployeeDTO(int id, String username, String password, boolean isActive, Set<RefundRequestDTO> refundRequests) {
+public record EmployeeDTO(int id, String username, String password, boolean isActive, Set<GameRequestDTO> gameRequests, Set<RefundRequestDTO> refundRequests) {
     public EmployeeDTO(Employee employee) {
         this(employee.getId(), employee.getUsername(), employee.getPassword(), employee.isActive(),
+                employee.getCopyGameRequests().stream().map(GameRequestDTO::new).collect(Collectors.toSet()),
                 employee.getCopyRefundRequests().stream().map(RefundRequestDTO::new).collect(Collectors.toSet()));
     }
     public Employee toEmployee() {
