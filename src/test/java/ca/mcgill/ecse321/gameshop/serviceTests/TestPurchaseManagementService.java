b06f235996a3e55fb79d07801806e33c360036dd
claraMickail <123259087+claraMickail@users.noreply.github.com>
2024-11-14 00:28:14 -0500

    fixed another minor bug

--- src/test/java/ca/mcgill/ecse321/gameshop/serviceTests/TestPurchaseManagementService.java
+++ src/test/java/ca/mcgill/ecse321/gameshop/serviceTests/TestPurchaseManagementService.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-public class testPurchaseManagementService {
+public class TestPurchaseManagementService {
 
     @InjectMocks
     PurchaseManagementService purchaseManagementService;
