44aec05e339127b43da4fecbf3a2053147cf646a
anagordon <anbgordon@gmail.com>
2024-12-04 23:18:43 -0500

    home page done

--- Gameshop-Frontend/src/views/HomeView.vue
+++ Gameshop-Frontend/src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script>
-import TheWelcome from '../components/TheWelcome.vue';
+<script setup>
 
 import {ref} from 'vue'
 
-const slideInterval = ref(5000)
+import ad1 from '../assets/ad1.png';
+import ad2 from '../assets/ad2.png';
+import ad3 from '../assets/ad3.png';
+import ad4 from '../assets/ad4.png';
+
+const slideInterval = ref(2500)
 </script>
 
 <template>
@@ -11,16 +15,28 @@ const slideInterval = ref(5000)
       <div class="display-6 my-3" align="center">
         Browse GameChest's Games Today
       </div>
+      
+       
+      <img :src="ad4" class="d-block w-100 rounded" alt="ad4" fit>
 
-      <template>
-        <BCarousel :interval="slideInterval" controls indicators ride="carousel">
-          <BCarouselSlide img-src="https://picsum.photos/1024/480/?image=19" />
-          <BCarouselSlide img-src="https://picsum.photos/1024/480/?image=20" />
-          <BCarouselSlide img-src="https://picsum.photos/1024/480/?image=21" />
-        </BCarousel>
-
-      </template>
+      <br>
 
   </div>
 
 </template>
+
+
+<style scoped>
+.mainContainer {
+  padding: 20px;
+}
+
+.rounded-carousel {
+  border-radius: 15px;
+  overflow: hidden;
+}
+
+.rounded-carousel .carousel-inner {
+  border-radius: 15px;
+}
+</style>
